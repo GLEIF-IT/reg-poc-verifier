25c56194e112ab3072bb331e4f2b14674ff728c1
pfeairheller <pfeairheller@gmail.com>
2023-06-21 13:42:07 -0700

    Adding full set of code comments

--- src/verifier/core/authorizing.py
+++ src/verifier/core/authorizing.py
@@ -12,14 +12,29 @@ from keri import kering
 from keri.core import coring
 from keri.help import helping
 
+
+# Hard-coded vLEI Engagement context role to accept.  This would be configurable in production
 EBA_DOCUMENT_SUBMITTER_ROLE = "EBA Document Submitter"
 
 
+# Hard coded credential JSON Schema SAID for the vLEI Engagement Context Role Credential
 class Schema:
     ECR_SCHEMA_SAID = "EEy9PkikFcANV1l7EHukCeXqrzT1hNZjGlUk7wuMO5jw"
 
 
 def setup(hby, vdb, reger, cf):
+    """
+
+    Parameters:
+        hby (Habery): key store and key log database environment
+        vdb (VerifierBaser): verifier specific database for storing accepted credentials and AIDs
+        reger (Reger): credential registry database environment
+        cf (Configer): configuation file loaded at initialization
+
+    Returns:
+        list: list of doers (coroutines) to run for this module
+
+    """
     data = dict(cf.get())
     if "LEIs" not in data:
         raise kering.ConfigurationError("invalid configuration, no LEIs available to accept")
@@ -30,6 +45,7 @@ def setup(hby, vdb, reger, cf):
 
     authorizer = Authorizer(hby, vdb, reger, leis)
 
+    # These lines representing AID keystate and credential revocation state monitoring.
     # witq = agenting.WitnessInquisitor(hby=hby)
     # monitor = Monitorer()
 
@@ -38,9 +54,8 @@ def setup(hby, vdb, reger, cf):
 
 class Authorizer:
     """
-    Authorizer is responsible for comminucating the receipt and successful verification
-    of credential presentation and revocation messages from external third parties via
-    web hook API calls.
+    Authorizer is responsible for comminucating the receipt and verification of credential
+    presentation and revocation messages from external third parties via web API calls.
 
 
     """
@@ -49,7 +64,7 @@ class Authorizer:
 
     def __init__(self, hby, vdb, reger, leis):
         """
-        Create a communicator capable of persistent processing of messages and performing
+        Create a Authenticator capable of persistent processing of messages and performing
         web hook calls.
 
         Parameters:
@@ -67,6 +82,12 @@ class Authorizer:
         self.clients = dict()
 
     def processPresentations(self):
+        """ Loop over any credential presentations in the iss database.
+
+        Credential presentations are placed in the iss database and this loop processes them, first checking to see
+        if the credential has been cryptographically verified then funning the EBA specific business logic.
+
+        """
 
         for (said,), dater in self.vdb.iss.getItemIter():
             # cancel presentations that have been around longer than timeout
@@ -86,6 +107,16 @@ class Authorizer:
                         print(f"invalid credential presentation, schema {creder.schema}")
 
     def processEcr(self, creder):
+        """ Process a fully verified engagement context role vLEI credential presentation
+
+        1.  Ensure the LEI is in the list of acceptable LEIs
+        2.  Ensure the role matches the required role for submission
+        3.  Save the credential as successful for submission acceptance.
+
+        Parameters:
+            creder (Creder):  Serializable credential object
+
+        """
         if creder.subject["i"] not in self.hby.kevers:
             print(f"unknown presenter {creder.subject['i']}")
             return
@@ -107,6 +138,11 @@ class Authorizer:
         self.vdb.accts.pin(keys=(kever.serder.pre,), val=creder.saider)
 
     def processRevocations(self):
+        """ Loop over database of credential revocations.
+
+        Successful verification of revocation removes submission authorization for the holder's AID
+
+        """
 
         for (said,), dater in self.vdb.rev.getItemIter():
 
@@ -142,8 +178,14 @@ class Authorizer:
 
 
 class AuthorizationDoer(doing.Doer):
+    """ Doer (coroutine) responsible for recurringly execute escrow process for credential processing """
 
     def __init__(self, authn):
+        """ Create coroutine for processing authorizer escrows
+
+        Parameters:
+            authn (Authorizer):  Authorizer to process escrows for
+        """
         self.authn = authn
         super(AuthorizationDoer, self).__init__()
 
--- src/verifier/core/verifying.py
+++ src/verifier/core/verifying.py
@@ -46,14 +46,60 @@ def loadEnds(app, hby, vdb, tvy, vry):
 
 
 class PresentationResourceEndpoint:
+    """ Credential presentation resource endpoint class
+
+    This class allows for a PUT to a credential SAID specific endpoint to trigger credential presentation
+    verification.
+
+    """
 
     def __init__(self, hby, vdb, tvy, vry):
+        """ Create credential presentation resource endpoint instance
+
+        Parameters:
+            hby (Habery): Database environment for exposed KERI AIDs
+            vdb (VerifierBaser): Verifier database environment
+            tvy (Tevery): transaction event log event processor
+            vry (Verifier): credential verification event processor
+
+        """
         self.hby = hby
         self.vdb = vdb
         self.tvy = tvy
         self.vry = vry
 
     def on_put(self, req, rep, said):
+        """  Credential Presentation Resource PUT Method
+
+        Parameters:
+            req: falcon.Request HTTP request
+            rep: falcon.Response HTTP response
+            said: qb64 SAID of credential being presented
+
+        ---
+         summary: Present vLEI ECR credential for AID authorization to other endpoints
+         description: Present vLEI ECR credential for AID authorization to other endpoints
+         tags:
+            - Credentials
+         parameters:
+           - in: path
+             name: said
+             schema:
+                type: string
+             description: qb64 SAID of credential being presented
+         requestBody:
+             required: true
+             content:
+                application/json+cesr:
+                  schema:
+                    type: application/json
+                    format: text
+         responses:
+           202:
+              description: Credential Presentation accepted
+
+        """
+
         if req.content_type not in ("application/json+cesr",):
             raise falcon.HTTPBadRequest(description=f"invalid content type={req.content_type} for VC presentation")
 
@@ -88,20 +134,49 @@ class PresentationResourceEndpoint:
 
 
 class AuthorizationResourceEnd:
+    """ Authroization resource endpoint
+
+    This resource endpoint class provides a GET method for verifying if an AID has
+     previously presented a valid vLEI ECR credential.
+
+    """
 
     def __init__(self, hby, vdb):
+        """ Create authorization resource endpoint
+
+        Parameters:
+            hby (Habery): Database environment for exposed KERI AIDs
+            vdb (VerifierBaser): Verifier database environment
+        """
         self.hby = hby
         self.vdb = vdb
 
     def on_get(self, req, rep, aid):
-        """
+        """  Authorization Resource GET Method
 
         Parameters:
-            req (Request): falcon HTTP request object
-            rep (Respose): falcon HTTP response object
-            aid (str): qb64 identifier to check
+            req: falcon.Request HTTP request
+            rep: falcon.Response HTTP response
+            aid: qb64 identifier prefix of presenter to check
 
-        Returns:
+        ---
+         summary:
+         description: Verifies is a given AID has previously submitted a valid vLEI ECR credential.
+         tags:
+            - Authorizations
+         parameters:
+           - in: path
+             name: aid
+             schema:
+                type: string
+             description: qb64 AID of presenter
+         responses:
+           200:
+              description: AID is authorized to sign requests
+           404:
+              description: AID has never presented any credentials
+           403:
+              description: AID has presented an invalid or subsequently revoked credential
 
         """
         if aid not in self.hby.kevers:
@@ -120,12 +195,54 @@ class AuthorizationResourceEnd:
 
 
 class RequestVerifierResourceEnd:
+    """ Request Verifier Resource endpoint class
+
+    This class provides a POST method endpoint that validating HTTP request signatures for AIDs that have previously
+    presented a valid vLEI credential.
+
+    """
 
     def __init__(self, hby, vdb):
+        """ Create a request verifier resource endpoint class
+
+        Parameters:
+            hby (Habery): Database environment for exposed KERI AIDs
+            vdb (VerifierBaser): Verifier database environment
+
+        """
         self.hby = hby
         self.vdb = vdb
 
     def on_post(self, req, rep, aid):
+        """  Request verifier resource POST method
+
+        Parameters:
+            req: falcon.Request HTTP request
+            rep: falcon.Response HTTP response
+            aid: qb64 identifier prefix of presenter to check
+
+        ---
+         summary:
+         description: Verifies the signature of request values for authorized AIDs
+         tags:
+            - Authorizations
+         parameters:
+           - in: path
+             name: aid
+             schema:
+                type: string
+             description: qb64 AID of presenter
+         responses:
+           200:
+              description: AID is authorized to sign requests
+           404:
+              description: AID has never presented any credentials
+           403:
+              description: AID has presented an invalid or subsequently revoked credential
+           401:
+              description: provided signature is not valid against values of the request
+
+        """
         data = req.params.get("data")
         sig = req.params.get("sig")
 
